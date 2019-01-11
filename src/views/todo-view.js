dba45cf9ded995d6931555f826569c8c60c9f211
Marcus Hellberg <marcus@vaadin.com>
2019-01-11 13:26:40 -0800

    Update to lit-element 2

--- src/views/todo-view.js
+++ src/views/todo-view.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from '@polymer/lit-element';
+import { LitElement, html } from 'lit-element';
 import '@vaadin/vaadin-text-field';
 import '@vaadin/vaadin-button';
 import '@vaadin/vaadin-checkbox';
